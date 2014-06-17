69bc53dfcec7679a9a4e06bc356d324f26cc47c1
wu06617106 <wu06617106@gmail.com>
2014-06-17 20:38:11 +0800

    first

---
+++ cvdrone-master/src/ardrone/resource.h
@@ -0,0 +1,25 @@
+#ifndef RESOURCE_H_INCLUDED
+
+#define RESOURCE_H_INCLUDED
+
+#include <windows.h>
+
+#define INP_BUFFER_SIZE 16384
+
+#define IDC_RECORD 1
+
+#define IDC_PLAY 2
+
+#define IDC_STOP 3
+
+#define NUM 20000
+
+//defines for menu
+
+#define APP_SAVE 1003
+
+#define APP_EXIT  1004
+
+ 
+
+#endif // RESOURCE_H_INCLUDED
